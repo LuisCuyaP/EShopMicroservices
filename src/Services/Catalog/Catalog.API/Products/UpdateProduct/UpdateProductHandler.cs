707dee68838329c47dbdae122495c7a774894fa7
Luis Cuya <luis.cuya@intelica.com>
2025-05-01 21:34:59 -0500

    Se retira codigo repetittivo en los services para logger

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -14,11 +14,10 @@ public class UpdateProductCommandValidator : AbstractValidator<UpdateProductComm
             .GreaterThan(0).WithMessage("Price must be greater than 0");
     }
 }
-internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+internal class UpdateProductCommandHandler(IDocumentSession session) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
 {
     public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
-    {
-        logger.LogInformation("UpdateProductHandler.Handle calle with {@Command}", command);
+    {       
         var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
         if (product is null)
         {            
